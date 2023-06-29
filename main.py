b64bf3a1923ae6a2699a9fe2199c4baa9214f3f3
andreycoutt <contato.andreyyy@gmail.com>
2023-06-28 21:08:18 -0300

    ..

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from vosk import Model, KaldiRecognizer
 import os
 import pyaudio
+import pyttsx3
+import json
+
+#Sintese de fala
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-2].id)
+
+def speak(text):
+    engine.say(text)
+engine.runAndWait()
+
+
+
 
 model = Model('model')
 rec = KaldiRecognizer(model, 16000)
@@ -10,12 +25,16 @@ stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, fram
 stream.start_stream()
 
 while True:
-    data = stream.read(4000)
+    data = stream.read(8192)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
+        result = rec.Result()
+        result = json.loads(result)
 
-print(rec.FinalResult)
+        if result is not None:
+            text  =  result['text']
+        
+        
+        print(text)
+        speak(text)
